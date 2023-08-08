20e3b82fc3e15fc06a888e936fbe09a447cee522
Nikita Shreshta <nikshres522@gmail.com>
2023-08-07 20:02:08 -0500

    main: Fixed port binding issue

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,4 +10,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='3111')
+    port = int(os.environ.get('PORT', 3111))
+    app.run(host='0.0.0.0', port=port)
